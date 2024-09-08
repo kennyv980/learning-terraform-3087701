7150e80f5293ab1f0066b38ece5570baf595c2d4
Kenny Veraha <145813493+kennyv980@users.noreply.github.com>
2024-09-07 22:11:03 -0400

    last fix

--- main.tf
+++ main.tf
@@ -49,11 +49,11 @@ resource "aws_security_group_rule" "web_http_in" {
 }
 
 resource "aws_security_group_rule" "web_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.web.id
 }
