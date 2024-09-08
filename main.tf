36da404c5c945588e13ce6b722eb90021992266e
Kenny Veraha <145813493+kennyv980@users.noreply.github.com>
2024-09-08 10:16:49 -0400

    fix security group

--- main.tf
+++ main.tf
@@ -36,51 +36,13 @@ module "web_sg" {
   name    = "web_new"
 
   vpc_id  = data.aws_vpc.default.id
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
+
+  ingress_rules      = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_block = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
-  egress_cidr_block = ["0.0.0.0/0"]
+  egress_rules       = ["all-all"]
+  egress_cidr_block  = ["0.0.0.0/0"]
 }
-
-resource "aws_security_group" "web" {
-  name        = "web"
-  description = "Allow http and https in. Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
-}
-
-
-resource "aws_security_group_rule" "web_http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-resource "aws_security_group_rule" "web_https_in" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-}
-
-  resource "aws_security_group_rule" "web_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.web.id
-  }
-
 
 
   
