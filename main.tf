2de3a95b42e38b91ece7f295ee52e08ea59efdf8
Kenny Veraha <145813493+kennyv980@users.noreply.github.com>
2024-09-07 22:04:04 -0400

    fix 6

--- main.tf
+++ main.tf
@@ -22,8 +22,7 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [aws_security_group_web.id]
-  
+  vpc_security_group_ids = [aws_security_group.web.id]
 
   tags = {
     Name = "HelloWorld"
@@ -36,7 +35,6 @@ resource "aws_security_group" "web" {
 
   vpc_id = data.aws_vpc.default.id
 }
-
 
 resource "aws_security_group_rule" "web_http_in" {
   type        = "ingress"
@@ -58,7 +56,7 @@ resource "aws_security_group_rule" "web_https_in" {
   security_group_id = aws_security_group.web.id
 }
 
-  resource "aws_security_group_rule" "web_everything_out" {
+resource "aws_security_group_rule" "web_everything_out" {
   type        = "egress"
   from_port   = 0
   to_port     = 0
@@ -66,4 +64,4 @@ resource "aws_security_group_rule" "web_https_in" {
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.web.id
-  }
+}
