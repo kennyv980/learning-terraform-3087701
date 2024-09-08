022f01a1ad80076a0d493bfc6205268417274398
Kenny Veraha <145813493+kennyv980@users.noreply.github.com>
2024-09-08 14:52:46 -0400

    test 13

--- main.tf
+++ main.tf
@@ -40,21 +40,21 @@ resource "aws_instance" "web" {
   }
 }
 
-module "blog_alb" {
+module "web_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog-alb"
+  name = "web-alb"
 
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+  vpc_id             = module.web_vpc.vpc_id
+  subnets            = module.web_vpc.public_subnets
+  security_groups    = [module.web_sg.security_group_id]
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "web-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
