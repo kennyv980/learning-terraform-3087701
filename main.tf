a7e0c693b28faa9d544a7f24ce2bdfe3b2559ded
Kenny Veraha <145813493+kennyv980@users.noreply.github.com>
2024-09-11 20:57:50 -0400

    Refactor to use variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,21 +11,21 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 module "web_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets = [${var.environment.network_prefix}.101.0/24", ${var.environment.network_prefix}.102.0/24", ${var.environment.network_prefix}.103.0/24"]
 
   tags = {
     Terraform   = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
@@ -33,9 +33,9 @@ module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.5.2"
 
-  name     = "web"
-  min_size = 1
-  max_size = 2
+  name     = "${var.environment.name}-web"
+  min_size = var.asg_min_size
+  max_size = var.asg_max_size
   
   vpc_zone_identifier = module.web_vpc.public_subnets
   target_group_arns   = module.web_alb.target_group_arns
@@ -50,7 +50,7 @@ module "web_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "web-alb"
+  name = "${var.environment.name}-web-alb"
 
   load_balancer_type = "application"
 
@@ -60,7 +60,7 @@ module "web_alb" {
 
   target_groups = [
     {
-      name_prefix      = "web-"
+      name_prefix      = "${var.environment.name}."
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -76,14 +76,14 @@ module "web_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 
 module "web_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.2.0"
-  name    = "web"
+  name    = "${var.environment.name}-web"
   vpc_id  = module.web_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
